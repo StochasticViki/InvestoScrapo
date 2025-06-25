75b1a1877b8526ec6568af1cc0c05dbb96ac016b
StochasticViki <vikigamer21@gmail.com>
2025-06-25 15:51:17 +0530

    Modular Python tool to extract and clean historical stock data from Investing.com
    InvestoScrapo is a Python-based data extraction and cleaning tool designed for equity analysts, quants, and valuation professionals. It automates the process of:
    
    Searching stock symbols on Investing.com
    
    Fetching historical stock price data with retries and anti-bot handling
    
    Cleaning and transforming the output into a structured time-series DataFrame
    
    This project is built with modularity and extensibility in mind and follows a production-grade project layout with support for parallel requests, error handling, and configurable headers.

---
+++ README.md
@@ -0,0 +1,32 @@
+# InvestoScrapo
+
+A fast, multi-threaded web scraper to collect historical financial data from Investing.com, built with curl_cffi and pandas.
+
+## Installation
+
+To install with pip on macOS or Linux, run:
+
+```bash
+    python3 -m pip install investoscrapo
+
+To install with pip on Windows, run:
+
+```bash
+    py -m pip install investoscrapo
+
+## Quickstart Guide
+
+```bash
+from investoscrapo.client import InvestingClient
+
+client = InvestingClient()
+query = "Reliance Industries Limited"
+search_results = client.Search(query)
+prices = client.Download_Historical(search_results, "2020-01-01", "2024-01-01")
+
+print(prices.head())
+
+
+## Contribute
+
+If you'd like to contribute to InvestoScrapo, check out https://github.com/StochasticViki/investoscrapo
---
+++ tests/test_investoscrapo.py
@@ -0,0 +1,15 @@
+from __future__ import division, print_function
+# import pytest
+import investoscrapo
+
+sample_list = [{'id': 9235, 'url': '/equities/reliance-steel---aluminum-co.', 'description': 'Reliance Steel & Aluminum Co', 'symbol': 'RS', 'exchange': 'NYSE', 'flag': 'USA', 'type': 'Stock - NYSE'}, 
+                 {'id': 18367, 'url': '/equities/reliance-industries', 'description': 'Reliance Industries Ltd', 'symbol': 'RELI', 'exchange': 'NSE', 'flag': 'India', 'type': 'Stock - NSE'}, 
+                 {'id': 1131514, 'url': '/equities/reliance-global', 'description': 'Reliance Global Group Inc', 'symbol': 'RELI', 'exchange': 'NASDAQ', 'flag': 'USA', 'type': 'Stock - NASDAQ'}]
+
+
+def test_basic():
+    pass  # TODO - add unit tests
+
+
+# if __name__ == "__main__":
+#     pytest.main()
